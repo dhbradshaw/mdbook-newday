b6907cd494f304ddfb53f8a96fc81b2affd2c05c
d. h. bradshaw <dhbradshaw@gmail.com>
2021-11-19 06:27:56 -0600

    let mdbook_summary_line_for_time accept different timezones

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
-use chrono::{Local, DateTime};
+use chrono::{Local, DateTime, TimeZone};
 
 pub fn now() -> DateTime<Local>{
     Local::now()
 }
 
-pub fn mdbook_summary_line_for_time(t: DateTime<Local>) -> String {
-    t.format("- [%A, %b %d, %Y](./%Y/%Y-%m/%Y-%m-%d.md)").to_string()
+pub fn mdbook_summary_line_for_time<T: TimeZone>(dt: DateTime<T>) -> String 
+    where T::Offset: std::fmt::Display {
+    dt.format("- [%A, %b %d, %Y](./%Y/%Y-%m/%Y-%m-%d.md)").to_string()
 }
 
 pub fn todays_line() -> String {
@@ -15,9 +16,13 @@ pub fn todays_line() -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use chrono::TimeZone;
 
     #[test]
-    fn it_works() {
-        println!("{}", todays_line());
+    fn summary_line() {
+        let dt: DateTime<Local> = Local.timestamp(1, 0);
+        println!("{}", mdbook_summary_line_for_time(dt));
+        println!("Today's line: {}", todays_line());
+
     }
 }
