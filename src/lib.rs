a2ba646f29c87ed91c9a2dc82b41633224c8e59e
d. h. bradshaw <dhbradshaw@gmail.com>
2021-11-19 07:48:51 -0600

    Idempotent even if line is without sigil

--- src/lib.rs
+++ src/lib.rs
@@ -19,18 +19,23 @@ pub fn todays_line() -> String {
 pub fn place_line_before(new_line: &str, sigil: &str, text: &str) -> String {
     let mut new_lines = vec![];
     let mut sigil_found = false;
+    let mut already_added = false;
     // Add the line before the first sigil if there is one.
     for text_line in text.lines() {
-        if text_line.starts_with(sigil) && !sigil_found {
+        if text_line == new_line {
+            already_added = true;
+        }
+        if text_line.starts_with(sigil) && !sigil_found && !already_added {
             if text_line != new_line {
                 new_lines.push(new_line);
+                already_added = true;
             }
             sigil_found = true;
         }
         new_lines.push(text_line);
     }
     // Add the line at the end if there is no sigil present.
-    if !sigil_found {
+    if !sigil_found && !already_added {
         new_lines.push(new_line);
     }
     new_lines.join("\n")
@@ -44,8 +49,9 @@ mod tests {
     #[test]
     fn test_place_line_before() {
         let line = "- [First!](./first.md)";
+        let line_without_sigil = "no sigil";
         let sigil = "- [";
-        let text_no_sigil = "[Intro](./intro)";
+        let text_without_sigil = "[Intro](./intro)";
         let text_with_sigil = "[Intro](./intro)\n- [Second!](./second.md)";
         let text_with_2_sigils = "[Intro](./intro)\n- [\n- [";
 
@@ -53,7 +59,7 @@ mod tests {
         let text = place_line_before(
             line,
             sigil,
-            text_no_sigil,
+            text_without_sigil,
         );
         assert_eq!(text, "[Intro](./intro)\n- [First!](./first.md)");
 
@@ -80,6 +86,21 @@ mod tests {
             text_with_2_sigils,
         );
         assert_eq!(text, "[Intro](./intro)\n- [First!](./first.md)\n- [\n- [");
+        
+        // A line without a sigil is still added idempotently.
+        let first_pass = place_line_before(
+            line_without_sigil,
+            sigil,
+            text_without_sigil,
+        );
+        assert_eq!(first_pass, "[Intro](./intro)\nno sigil");
+        let second_pass = place_line_before(
+            line_without_sigil,
+            sigil,
+            &first_pass,
+        );
+        assert_eq!(first_pass, second_pass)
+
     }
 
 
